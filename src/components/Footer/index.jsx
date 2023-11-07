a5c7f46914d35e393f2ef1834a977cd0e55d873f
Florian Kreuk <fkreuk@cartermario.com>
2023-11-07 13:23:22 -0500

    added footer with links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import GitHub from '../../assets/GitHub.png';
-import LinkedIn from '../../assets/LinkedIn.png';
+import LinkedIn from '../../assets/linkedIn.png';
+import '../../index.css';
 import './footer.css';
+
+
 
 export default function Footer() {
     return (
         <footer id="footer">
-            <div>
-                <a href="https://github.com/f-kreuk">
-                    <img src={GitHub} alt="GitHub"></img>
-                </a>
-                <a href="https://www.linkedin.com/in/florian-k-98031b50/">
-                    <img src={LinkedIn} alt="LinkedIn"></img>
-                </a>
+            <div className="row">
+                <div className="col-md-5"></div>
+                <div className="col-md-2 ml-5">
+                    <a href="https://github.com/f-kreuk">
+                        <img src={GitHub} className="icon my-2 p-1 justify-center" alt="GitHub"></img>
+                    </a>
+                    <a href="https://www.linkedin.com/in/florian-k-98031b50/">
+                        <img src={LinkedIn} className="icon my-2 p-1 justify-center" alt="LinkedIn"></img>
+                    </a>
+                </div>
+                <div className="col-md-5"></div>
             </div>
         </footer>
     )
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -20,4 +20,7 @@ export default function Home() {
         </div>
       </div>
   );
+
+  
+
 }
