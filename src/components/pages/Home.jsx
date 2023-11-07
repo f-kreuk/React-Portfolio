0ebe0b883c9c594446b26e9423d5f8ebf2f02258
Florian Kreuk <fkreuk@cartermario.com>
2023-11-07 14:03:18 -0500

    improved font and look of home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,22 +5,31 @@ import profilePicture from '../../assets/Florian.png';
 
 export default function Home() {
   return (
-    <div className="container row box">
+    <div className="row box">
+
       <div className="col-md-6 text-right my-auto">
         <div>
-          <h1>Florian Kreuk</h1>
+          <h1 className="signature">Florian Kreuk</h1>
           <p>
-          Attorney and Programmer
-        </p>
-        </div>
-        </div>
-        <div className="col-md-4">
-        <img src={profilePicture} alt="Florian Kreuk" />
-
+            Attorney and Programmer
+          </p>
+          <div className="row">
+          <div className="col-md-6"></div>
+          <div className="col-md-6">
+          <p className="ml-5">
+            Fueled by my unique blend of legal expertise and programming proficiency, I've honed my skills as a solutions architect specializing in PI law firms.
+          </p>
+          </div>
+          </div>
         </div>
       </div>
+      <div className="col-md-4">
+        <img src={profilePicture} alt="Florian Kreuk" />
+      </div>
+      <div className="col-md-2"></div>
+    </div>
   );
 
-  
+
 
 }
